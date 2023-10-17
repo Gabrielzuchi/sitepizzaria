803b0108ba2d7f6a3ae13d53791a63f5c3abbffd
Gabrielzuchi <gabrielzuchi10@gmail.com>
2023-10-17 13:07:31 -0300

    commit senai

---
+++ process/pizza.php
@@ -0,0 +1,26 @@
+<?php
+
+include_once("conn.php");
+
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+// Resgate dos dados, montagem do pedido
+if($method === "GET") {
+
+    $bordasQuery = $conn->query("SELECT * FROM borda;");
+    $bordas = $bordasQuery->fetchAll();
+
+    $massasQuery = $conn->query("SELECT * FROM massa;");
+    $massas = $massaQuery->fetchAll();
+
+    $saboresQuery = $conn->query("SELECT * FROM sabores;");
+    $sabores = $saboresQuery->fetchAll();
+
+
+    // Criação do pedido
+
+}else if($method === "POST") {
+
+
+
